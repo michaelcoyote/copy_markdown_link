3a971046b77671e2149da2ee48b4a0135c8fce46
michael guldan <michaelguldan@gmail.com>
2026-06-07 19:37:51 -0700

    Fix v0.4 problems and improve code
    Fix the tab group and all tabs functions broken in v0.4 update
    Add more command keys for tab group and all tab functions
    Continue to remove redundant code and split into functions

--- background.js
+++ background.js
@@ -22,32 +22,69 @@ chrome.contextMenus.create({
   id: "copy_markdown_group_tabs",
   title: "Links for Tab Group",
   contexts: ["page", "tab"],
-  enabled: false
+  enabled: false,
 });
 
-
+// Helper functions
+//
+// Write to clipboard
+//  handle promise from navigator.clipboard.writeText()
+function writeToClipboard(text) {
+  try{
+    navigator.clipboard.writeText(text);
+  } catch (err) {
+    console.error("Failed to write to clipboard: ", err);
+  }
+}
 // Link Functions
 //
+// Format markdown link
+function formatLink(tab) {
+  return `[${tab.title}](${tab.url})`;
+}
 // Single tab
 function copyMarkdownLinkForTab(tab) {
-    // Format the page title and URL into a Markdown link
-    const markdownLink = `[${tab.title}](${tab.url})`;
-    navigator.clipboard.writeText(markdownLink);
+  // Format the page title and URL into a Markdown link
+  return writeToClipboard(formatLink(tab));
 }
-// All tabs
-function copyMarkdownLinksForAllTabs() {
-    const markdownLinks = tabs.map((tab) => `- [${tab.title}](${tab.url})`).join('\n');
-    navigator.clipboard.writeText(markdownLinks);
-    console.log('Markdown links copied to clipboard!');
-}
-// Group
-function copyMarkdownLinksForGroup(tab) {
-    const markdownLinks = tabs.map((tab) => `- [${tab.title}](${tab.url})`).join('\n');
-    navigator.clipboard.writeText(markdownLinks);
-    console.log('Markdown links copied to clipboard!');
+// Multiple tabs
+function copyMarkdownLinksForMultipleTabs(tabs) {
+  console.debug(`Copying links to clipboard for ${tabs.length} tabs`);
+  return writeToClipboard(tabs.map((tab) => `- ${formatLink(tab)}`).join("\n"));
 }
 
 // Listeners
+//
+// Command listeners
+//
+// Keyboard Shortcut Page Link
+chrome.commands.onCommand.addListener((command, tab) => {
+  switch (command) {
+    case "copy_markdown_link_page_kbd":
+      console.debug("KBD command: copy_markdown_link_page_kbd");
+      copyMarkdownLinkForTab(tab);
+      break;
+    case "copy_markdown_all_tabs_kbd":
+      console.debug("KBD command: copy_markdown_all_tabs_kbd");
+      chrome.tabs.query({currentWindow: true}, (tabs) => {
+        copyMarkdownLinksForMultipleTabs(tabs);
+      });
+      break;
+    case "copy_markdown_group_tabs_kbd":
+      if(tab.groupId !== -1) {
+        console.debug("KBD command: copy_markdown_group_tabs_kbd");
+        chrome.tabs.query({currentWindow: true, groupId: tab.groupId}, (tabs) => {
+          copyMarkdownLinksForMultipleTabs(tabs);
+        });
+      } else {
+        console.log("No group selected");
+        return;
+      }
+      break;
+  }
+});
+//
+// Menu listeners
 //
 // Page Link
 chrome.contextMenus.onClicked.addListener((info, tab) => {
@@ -55,24 +92,18 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     copyMarkdownLinkForTab(tab);
   }
 });
-// Keyboard Shortcut Page Link
-chrome.commands.onCommand.addListener((command, tab) => {
-  if (command === "copy_markdown_link_page_kbd") {
-    copyMarkdownLinkForTab(tab);
-  }
-});
 // All Tabs
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "copy_markdown_all_tabs") {
     chrome.tabs.query({currentWindow: true}, (tabs) => {
-      copyMarkdownLinksForAllTabs(tabs);
+      copyMarkdownLinksForMultipleTabs(tabs);
     });
   }
 });
 // Group Tabs
-chrome.contextMenus.onShown.addListener(async (info, tab) => {
+chrome.contextMenus.onShown.addListener((info, tab) => {
   const enabled = tab.groupId !== -1;
-  await chrome.contextMenus.update("copy_markdown_group_tabs", {enabled});
+  chrome.contextMenus.update("copy_markdown_group_tabs", {enabled});
   chrome.contextMenus.refresh();
 });
 chrome.contextMenus.onClicked.addListener((info, tab) => {
@@ -81,11 +112,11 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     console.log(`Group ID:  ${groupId}`);
     if (groupId === -1) {
       // Handle ungrouped case or notify the user
-      console.log('No group selected');
+      console.log("No group selected");
       return;
-      }
+    }
     chrome.tabs.query({currentWindow: true, groupId}, (tabs) => {
-      copyMarkdownLinksForGroup(tab);
+      copyMarkdownLinksForMultipleTabs(tabs);
     });
   }
 });
